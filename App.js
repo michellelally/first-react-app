27ca863947de438289a8f34692cdb35ee0a6391e
Shelley <shelley.lally@gmail.com>
2021-03-09 18:00:08 +0000

    clicking button excercise complete

--- App.js
+++ App.js
@@ -13,24 +13,20 @@ import {
 const App = () => {
   
   // for dynamically changing text
-  const[name, setName] = useState('Pick Your Poison')
+  const[number, setNumber] = useState(0)
 
-  const[cocktail , setCocktail] = useState({ number: 6, title: 'Pornstar Martini'})
+  const[message , setMessage] = useState('You have clicked {number} times')
 
-  const[current, setCurrent] = useState(true)
 
   const onClickHandler = () => {
-    setName("Michelle")
-    setCocktail({number:7, title:'Espresso Martini'})
-    setCurrent(false)
+      setNumber(number + 1)
   }
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>You chose cocktail number {cocktail.number} which is {cocktail.title}</Text>
-      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
-      <Button title='Update State' onPress={onClickHandler}></Button> 
+      <Text style={styles.text}>{number}</Text>
+  <Text style={styles.text}>You have clicked {number} times</Text>
+      <Button title='Add' onPress={onClickHandler}></Button> 
     </View>
   );
 };
