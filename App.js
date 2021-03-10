ce5abc941a01826c0ae4147e00845f0fac69d023
Shelley <shelley.lally@gmail.com>
2021-03-10 16:42:56 +0000

    arrays, lists, scrollview, refresh control

--- App.js
+++ App.js
@@ -4,46 +4,54 @@ import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  Button
+  Text, 
+  ScrollView,
+  RefreshControl
 } from 'react-native';
 
 
 const App = () => {
 
-  return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
+  const [Items, setItems] = useState([
+    { key: 1, item: 'Item 1' },
+    { key: 2, item: 'Item 2' },
+    { key: 3, item: 'Item 3' },
+    { key: 4, item: 'Item 4' },
+    { key: 5, item: 'Item 5' },
+    { key: 6, item: 'Item 6' },
+    { key: 7, item: 'Item 1' },
+    { key: 8, item: 'Item 2' },
+    { key: 9, item: 'Item 3' },
+    { key: 10, item: 'Item 4' },
+    { key: 11, item: 'Item 5' },
+    { key: 12, item: 'Item 6' }
+  ]);
+  const [Refreshing, setRefreshing] = useState(false);
 
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </View>
+  const onRefresh = () => {
+    setRefreshing(true);
+    setItems([...Items, { key: 69, item: 'Item 69' }]);
+    setRefreshing(false);
+  }
+  return (
+      <ScrollView 
+      style={styles.body}
+      refreshControl={
+        <RefreshControl
+          refreshing={ Refreshing }
+          onRefresh= {onRefresh}
+        />
+      }>
+        {
+          Items.map((item) => {
+            return (
+              <View style={styles.item} key={item.key}>
+                <Text styles={styles.text}>{item.item}</Text>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
   );
 };
 
@@ -51,69 +59,18 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
+    backgroundColor: '#000000'
 
   },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#00ff00',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
+  item: {
+    margin: 10,
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  big_row: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
   text: {
-    color: '#fffff0',
-    fontSize: 20,
+    color: '#ffffff',
+    fontSize: 50,
     margin: 10,
   }
 });
