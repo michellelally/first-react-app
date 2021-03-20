38c9481df905d356a90b513193df2facfbabc085
Shelley <shelley.lally@gmail.com>
2021-03-20 23:03:31 +0000

    getting user input and displaying on screen

--- App.js
+++ App.js
@@ -5,92 +5,32 @@ import {
   StyleSheet,
   View,
   Text,
-  ScrollView,
-  RefreshControl,
-  FlatList,
-  SectionList
+  TextInput
 } from 'react-native';
 
 
 const App = () => {
+const [name, SetName] = useState('');
 
-  const [Sections, setSections] = useState([
-    {
-      title: 'Title 1',
-      data: ['Item 1-1', 'Item 1-2'],
-    }
-  ]);
-  const [Refreshing, setRefreshing] = useState(false);
-
-  const onRefresh = () => {
-    setRefreshing(true);
-    const adding_index = Sections.length + 1;
-    setSections([...Sections,
-    {
-      title: 'Title ' + adding_index,
-      data:
-        [
-          'Item ' + adding_index + '-1',
-          'Item ' + adding_index + '-2',
-        ],
-    }
-    ]);
-    setRefreshing(false);
-  }
 
   return (
-    <SectionList
-      keyExtractor={(item, index) => index.toString()}
-      sections={Sections}
-      renderItem={({ item }) => (
-        <Text style={styles.text}>{item}</Text>
-      )}
-      renderSectionHeader={({ section }) => (
-        <View style={styles.item}>
-          <Text style={styles.text}>{section.title}</Text>
-        </View>
-      )}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
-        />
-      }
-    />
+
+    <View style={styles.body}>
+      <Text style={styles.text}>
+        Please write your name:
+      </Text>
+      <TextInput
+        //multiline
+        style={styles.input}
+        placeholder='e.g John Smith'
+        onChangeText={(value) => SetName(value)}
+      />
+        <Text style={styles.text}>
+        Your name: {name}
+      </Text>
 
 
-    // <FlatList
-    //   keyExtractor={(item, index) => index.toString()}
-    //   data={Items}
-    //   renderItem={({ item }) => (
-    //     <View style={styles.item}>
-    //       <Text styles={styles.text}>{item.name}</Text>
-    //     </View>
-    //   )}
-
-    //   refreshControl={
-    //     <RefreshControl
-    //       refreshing={Refreshing}
-    //       onRefresh={onRefresh}
-    //     />
-    //   }
-
-    // />
-
-    // <ScrollView 
-    // style={styles.body}
-
-    // }>
-    //   {
-    //     Items.map((item) => {
-    //       return (
-    //         <View style={styles.item} key={item.key}>
-    //           <Text styles={styles.text}>{item.item}</Text>
-    //         </View>
-    //       )
-    //     })
-    //   }
-    // </ScrollView>
+    </View>
 
   );
 };
@@ -98,22 +38,20 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#000000'
-
-  },
-  item: {
-
-    margin: 10,
-    backgroundColor: '#4ae1fa',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
   },
   text: {
     color: '#000000',
-    fontSize: 50,
+    fontSize: 20,
     margin: 10,
-  }
+  },
+  input: {
+    borderWidth: 1,
+    width: 200,
+    borderColor: '#555',
+    borderRadius: 5,
+  },
 });
 
 export default App;
